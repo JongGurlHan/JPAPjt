aa358ad0e6184fb44742c8c325b6889c61fa3c93
JG HAN <gojkhan13@gmail.com>
2022-01-11 23:28:50 +0900

    0111 기본설정

---
+++ src/main/java/com/JGHan/JPAPjt/JpaPjtApplication.java
@@ -0,0 +1,13 @@
+package com.JGHan.JPAPjt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaPjtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaPjtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/JGHan/JPAPjt/JpaPjtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JGHan.JPAPjt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaPjtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
