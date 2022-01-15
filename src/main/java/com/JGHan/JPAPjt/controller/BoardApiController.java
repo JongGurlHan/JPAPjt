a467f4d0d3d6a45acb94a877b6c8499cfdfa3e07
JG HAN <gojkhan13@gmail.com>
2022-01-15 18:16:13 +0900

    0115 JPA이용한 RESTful API 작성

---
+++ src/main/java/com/JGHan/JPAPjt/controller/BoardApiController.java
@@ -0,0 +1,60 @@
+package com.JGHan.JPAPjt.controller;
+
+import java.util.List;
+
+import com.JGHan.JPAPjt.model.Board;
+import com.JGHan.JPAPjt.repository.BoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.thymeleaf.util.StringUtils;
+
+@RestController
+@RequestMapping("/api")
+class BoardApiController {
+
+    @Autowired
+    private BoardRepository boardRepository;
+
+    @GetMapping("/boards")
+    List<Board> all(@RequestParam(required = false, defaultValue = "") String title,
+                    @RequestParam(required = false, defaultValue = "") String content){
+        if(StringUtils.isEmpty(title) && StringUtils.isEmpty(content)){
+            return boardRepository.findAll();
+        }else{
+            return boardRepository.findByTitleOrContent(title, content);
+        }
+    }
+
+    @PostMapping("/boards")
+    Board newBoard(@RequestBody Board newBoard) {
+        return boardRepository.save(newBoard);
+    }
+
+    // Single item
+
+    @GetMapping("/boards/{id}")
+    Board one(@PathVariable Long id) {
+        return boardRepository.findById(id).orElse(null); //없으면 null반환
+    }
+
+
+    @PutMapping("/boards/{id}")
+    Board replaceBoard(@RequestBody Board newBoard, @PathVariable Long id) {
+
+        return boardRepository.findById(id)
+                .map(Board -> {
+                    Board.setTitle(newBoard.getTitle());
+                    Board.setContent(newBoard.getContent());
+                    return boardRepository.save(Board);
+                })
+                .orElseGet(() -> {
+                    newBoard.setId(id);
+                    return boardRepository.save(newBoard);
+                });
+    }
+
+    @DeleteMapping("/boards/{id}")
+    void deleteBoard(@PathVariable Long id) {
+        boardRepository.deleteById(id);
+    }
+}
--- src/main/java/com/JGHan/JPAPjt/repository/BoardRepository.java
+++ src/main/java/com/JGHan/JPAPjt/repository/BoardRepository.java
@@ -3,5 +3,11 @@ package com.JGHan.JPAPjt.repository;
 import com.JGHan.JPAPjt.model.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BoardRepository extends JpaRepository<Board, Long> {
+
+    //인터페이스만 정의하면 구현은 알아서 된다!!
+    List<Board> findByTitle(String title);
+    List<Board> findByTitleOrContent(String title, String content);
 }
